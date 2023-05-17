af9df6546a1c7ee04371f7a919297900580c3bdb
jiahua2023421 <131435308+jiahua2023421@users.noreply.github.com>
2023-05-17 13:35:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,30 +10,45 @@ from public import parse_args, log
 from data_process import show, save_result
 from model import DnCNN
 
+
+
 if __name__ == '__main__':
     # 参数
     args = parse_args(1)
 
-    # 加载模型
+    # model = DnCNN()
     if not os.path.exists(os.path.join(args.model_dir, args.model_name)):  # model_001若不存在，则加载model
+
+        model_cpu = torch.load(os.path.join(args.model_dir, 'model.pth'), map_location='cpu')  # 映射到CPU
         model = torch.load(os.path.join(args.model_dir, 'model.pth'), map_location='cpu')  # 映射到CPU
+        # load weights into new model
         log('load trained model on Train400 dataset by kai')
     else:
+        # model.load_state_dict(torch.load(os.path.join(args.model_dir, args.model_name)))
+        model_cpu = torch.load(os.path.join(args.model_dir, args.model_name), map_location='cpu')
         model = torch.load(os.path.join(args.model_dir, args.model_name), map_location='cpu')
         log('load trained model')
 
-    model.eval()  # evaluation mode  设置为预测模式
+    #    params = model.state_dict()
+    #    print(params.values())
+    #    print(params.keys())
+    #
+    #    for key, value in params.items():
+    #        print(key)    # parameter name
+    #    print(params['dncnn.12.running_mean'])
+    #    print(model.state_dict())
 
-    # GPU or CPU
-    #    if torch.cuda.is_available():  #GPU
-    #        model = model.cuda()
+    model_cpu.eval()  # evaluation mode  设置为预测模式
+    #    model.train()
 
-    # 保存路径
+    if torch.cuda.is_available():  #GPU
+        model = model.cuda()
+
     if not os.path.exists(args.result_dir):  # 结果路径
         os.mkdir(args.result_dir)  # 创造目录
 
-    # 测试图片保存
     for set_cur in args.set_names:
+
         if not os.path.exists(os.path.join(args.result_dir, set_cur)):
             os.mkdir(os.path.join(args.result_dir, set_cur))
         psnrs = []
@@ -43,6 +58,7 @@ if __name__ == '__main__':
             if im.endswith(".jpg") or im.endswith(".bmp") or im.endswith(".png"):
                 x = np.array(imread(os.path.join(args.set_dir, set_cur, im)), dtype=np.float32) / 255.0
                 #  dtype:数组中的数据类型
+
                 np.random.seed(seed=0)  # for reproducibility
                 y = x + np.random.normal(0, args.sigma / 255.0, x.shape)  # Add Gaussian noise without clipping
                 y = y.astype(np.float32)
@@ -50,8 +66,14 @@ if __name__ == '__main__':
 
                 torch.cuda.synchronize()
                 start_time = time.time()
-                #               y_ = y_.cuda()
-                x_ = model(y_)  # inference
+                # ceshi = y_.nelement
+                # ceshi2 = y_.squeeze(2)
+                ceshi = y.size
+                if y.size < 154402:
+                    y_ = y_.cuda()
+                    x_ = model(y_)  # inference
+                else:
+                    x_ = model_cpu(y_)
                 x_ = x_.view(y.shape[0], y.shape[1])
                 x_ = x_.cpu()
                 x_ = x_.detach().numpy().astype(np.float32)
@@ -63,7 +85,7 @@ if __name__ == '__main__':
                 ssim_x_ = compare_ssim(x, x_)
                 # if args.save_result:
                 name, ext = os.path.splitext(im)
-                show(np.hstack((y, x_)))  # show the image
+                # show(np.hstack((y, x_)))  # show the image
                 save_result(x_, path=os.path.join(args.result_dir, set_cur,
                                                   name + '_dncnn' + ext))  # save the denoised image
                 psnrs.append(psnr_x_)
