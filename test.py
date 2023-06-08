56e92e5d49194235cac2fedcc2f8c1a5dc590dbd
jiahua2023421 <1747702371@qq.com>
2023-06-08 20:41:17 +0800

    deamnet测试结果

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import torch
 import cv2
 import torch.nn as nn
 import argparse
+import pandas as pd
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 import numpy as np
 from imageio import imread
@@ -124,10 +125,11 @@ parser = argparse.ArgumentParser(description="AWGN Testing......")
 parser.add_argument("--pretrained", type=str, default="./Deam_models/", help='path of log files')
 parser.add_argument("--test_data", type=str, default='Set12', help='test dataset such as Set12, Set68 and Urban100')
 parser.add_argument("--test_noiseL", type=float, default=15, help='noise level used on test set')
-
+parser.add_argument('--statistics', default='./statistics/', help='Location to save statistics')
 parser.add_argument('--data_dir', type=str, default='./data')
+parser.add_argument('--module', type=str, default='noise15.pth')
 parser.add_argument('--Isreal', default=False, help='If training/testing on RGB images')
-parser.add_argument('--result_dir', default='results1', type=str, help='directory of test dataset')#测试结果目录
+parser.add_argument('--result_dir', default='deam_results/sigma15', type=str, help='directory of test dataset')#测试结果目录
 opt = parser.parse_args()
 
 
@@ -154,7 +156,7 @@ def main():
     print('Loading model ...\n')
     net = Deam(opt.Isreal)
     model = nn.DataParallel(net).cuda()
-    model.load_state_dict(torch.load(os.path.join(opt.pretrained, 'noise15.pth'), map_location=lambda storage, loc: storage))
+    model.load_state_dict(torch.load(os.path.join(opt.pretrained, opt.module), map_location=lambda storage, loc: storage))
     model.eval()
 
     # print('Loading data info ...\n')
@@ -171,7 +173,7 @@ def main():
         SEED = 0
         torch.manual_seed(SEED)
         torch.cuda.manual_seed(SEED)
-
+        start_time = time.time()
         image_path = os.path.join(files_path, f)
         # image
         Img = cv2.imread(image_path)
@@ -218,7 +220,8 @@ def main():
         # ssim_x_ = compare_ssim(x, x_)
         psnrs.append(psnr_x_)  # 向列表末尾添加元素
         ssims.append(ssim_x_)
-
+        elapsed_time = time.time() - start_time
+        print(' %10s : %2.4f second' % (f, elapsed_time))
     psnr_avg = np.mean(psnrs)  # np.mean求平均值
     ssim_avg = np.mean(ssims)
     psnrs.append(psnr_avg)
@@ -228,6 +231,10 @@ def main():
     #         # 以文本形式 保存每一张图片的PSNR与SSIM结果
     log('Datset: {0:10s} \n  PSNR = {1:2.2f}dB, SSIM = {2:1.4f}'.format(opt.result_dir, psnr_avg, ssim_avg))
     print("PSNR on test data %f" % psnr_test)
+    # data_frame = pd.DataFrame(
+    #     data={'data': opt.test_data, 'sigma': opt.test_noiseL, 'PSNR': psnr_avg, 'SSIM': ssim_avg}, index=range(1,   2)
+    # )
+    # data_frame.to_csv(os.path.join(opt.statistics, 'testing_result.csv'), index_label='index')
     print("\n")
 
 
